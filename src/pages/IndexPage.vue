4c5cb916ca4b43e38dfc720811c0bd1c061f1a5d
Panuwatttt <fait88392qq@gmail.com>
2023-10-06 18:51:17 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+# IndexPage.vue
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'Panuwat Prasert 6504101369',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
